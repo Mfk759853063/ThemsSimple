44f94f3baeabddb17fc219acb66ec64b40eecff7
周灿 <6228851057209164>
2016-03-18 11:23:28 +0800

    Initial Commit

---
+++ KNThemSimple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNThemSimple
+//
+//  Created by vbn on 16/3/18.
+//  Copyright © 2016年 iClip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
